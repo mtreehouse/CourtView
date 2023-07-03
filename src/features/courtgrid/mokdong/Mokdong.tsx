fa2f4868285d76af45a8a46b09af3f4e6a2d475e
mtree <sym3927@naver.com>
2023-07-03 17:59:35 +0900

    solving react component in gridjs problem

--- src/features/courtgrid/mokdong/Mokdong.tsx
+++ src/features/courtgrid/mokdong/Mokdong.tsx
@@ -3,7 +3,8 @@ import { useDispatch } from 'react-redux'
 import {getMonthTimeState} from 'features/courtgrid/courtAPI'
 import {PlaceState, DateState} from 'models'
 import {DateTime} from 'ts-luxon'
-import { Grid, _ } from "gridjs-react";
+import { Grid } from "gridjs-react";
+import { html } from "gridjs";
 import "gridjs/dist/theme/mermaid.css";
 import styles from 'features/courtgrid/CourtGrid.module.css';
 import Accordion from '@mui/material/Accordion';
@@ -166,6 +167,13 @@ export function Mokdong() {
   
   return (
     <div>
+          <Button
+            color="success"
+            size="small"
+            variant="outlined"
+            startIcon={<SportsTennisOutlinedIcon />}
+            style={{display:'none'}}
+          />
       <div className='t-r w-100'>
         <Button onClick={()=>{}}>시간 내림차순</Button>
         <Button onClick={(e)=>{expandOnclick(expanded);}}>{expanded ? EXPAND_BTN_TXT.COLLAPESE : EXPAND_BTN_TXT.EXPAND}</Button>
@@ -199,20 +207,7 @@ export function Mokdong() {
                   },
                   {
                     name: "예약",
-                    formatter: (cell: any, row: any) =>{
-                      _(
-                        <Button
-                          color="success"
-                          size="small"
-                          variant="outlined"
-                          onClick={() => {
-                            copy(row._cells);
-                          }}
-                          startIcon={<SportsTennisOutlinedIcon />}
-                        >
-                          {" "}
-                        </Button>
-                      )},
+                    formatter: (_: any, row: any) => html('<div><button onClick="alert("asdf")" class="MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedSuccess MuiButton-sizeSmall MuiButton-outlinedSizeSmall MuiButton-root MuiButton-outlined MuiButton-outlinedSuccess MuiButton-sizeSmall MuiButton-outlinedSizeSmall css-1k9gzqu-MuiButtonBase-root-MuiButton-root" tabindex="0" type="button"><span class="MuiButton-startIcon MuiButton-iconSizeSmall css-y6rp3m-MuiButton-startIcon"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SportsTennisOutlinedIcon"><path d="M19.52 2.49C17.18.15 12.9.62 9.97 3.55c-1.6 1.6-2.52 3.87-2.54 5.46-.02 1.58.26 3.89-1.35 5.5l-4.24 4.24 1.42 1.42 4.24-4.24c1.61-1.61 3.92-1.33 5.5-1.35s3.86-.94 5.46-2.54c2.92-2.93 3.4-7.21 1.06-9.55zm-9.2 9.19c-1.53-1.53-1.05-4.61 1.06-6.72s5.18-2.59 6.72-1.06c1.53 1.53 1.05 4.61-1.06 6.72s-5.18 2.59-6.72 1.06zM18 17c.53 0 1.04.21 1.41.59.78.78.78 2.05 0 2.83-.37.37-.88.58-1.41.58s-1.04-.21-1.41-.59c-.78-.78-.78-2.05 0-2.83.37-.37.88-.58 1.41-.58m0-2c-1.02 0-2.05.39-2.83 1.17-1.56 1.56-1.56 4.09 0 5.66.78.78 1.81 1.17 2.83 1.17s2.05-.39 2.83-1.17c1.56-1.56 1.56-4.09 0-5.66C20.05 15.39 19.02 15 18 15z"></path></svg></span><span class="MuiTouchRipple-root css-8je8zh-MuiTouchRipple-root"></span></button></div>')
                   },
                   {
                     id: "place_cd",
